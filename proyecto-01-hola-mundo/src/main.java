c2d8dc990edf10f284534a273955240f0592ce15
Tu Gxelds3 <20253ds080@utez.edu.mx>
2026-01-12 09:00:14 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("Aguilar Hernandez Judith Desiree 2D");
+        System.out.println("12 de enero del 2026");
+    }
+}
