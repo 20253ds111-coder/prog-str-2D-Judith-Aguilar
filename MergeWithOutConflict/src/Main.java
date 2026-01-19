4fd2fb38033f685052e028e0c8e2255ad84c9a1d
20253ds111-coder <20253ds111@utez.edu.mx>
2026-01-18 19:24:30 -0600

    feat: agregar operaciones

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Semana 2");
         System.out.println("Versión base");
+
+        System.out.println("2 + 2 = 4");
+        System.out.println("5 * 3 = 15");
     }
 }
